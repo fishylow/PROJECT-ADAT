dc8c247d9bd130b5607bb70bd3b4e198f181c8df
diegowh <contacto.diegowh@gmail.com>
2023-10-03 21:58:28 +0200

    Update return type for get_monitors_resolution
    Now matches with the return

--- monitors.py
+++ monitors.py
@@ -5,7 +5,7 @@ class MonitorController:
     def __init__(self) -> None:
         self.monitors_info = get_monitors()
 
-    def get_monitors_resolution(self) -> list[tuple]:
+    def get_monitors_resolution(self) -> dict:
         monitors = {}
         for i, monitor in enumerate(self.monitors_info):
             monitor_name = f"DISPLAY{i+1}" # Para que el nombre del monitor coincida con el de Windows
