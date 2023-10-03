e291f707de50c48af311b3ccbf1aeb93510c4030
diegowh <contacto.diegowh@gmail.com>
2023-10-03 21:59:39 +0200

    Fix an error with the files
    Fixed duplicated file monitor_controller.py and monitors.py

--- monitor_controller.py
+++ monitor_controller.py
@@ -5,5 +5,10 @@ class MonitorController:
     def __init__(self) -> None:
         self.monitors_info = get_monitors()
 
-    def get_monitors_resolution(self) -> list[tuple]:
-        return [(monitor.width, monitor.height) for monitor in self.monitors_info]
+    def get_monitors_resolution(self) -> dict:
+        monitors = {}
+        for i, monitor in enumerate(self.monitors_info):
+            monitor_name = f"DISPLAY{i+1}" # Para que el nombre del monitor coincida con el de Windows
+            monitors[monitor_name] = {"width": monitor.width, "height": monitor.height}
+
+        return monitors
